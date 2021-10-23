9f64155b23c72bad9ece1fa01bd0c1654c731ce1
ianppaquette <55311071+ianppaquette@users.noreply.github.com>
2021-10-23 10:37:43 -0700

    Add files via upload

---
+++ src/Plugin.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "zeek/plugin/Plugin.h"
+
+namespace plugin {
+namespace Local_coap {
+
+class Plugin : public zeek::plugin::Plugin
+{
+protected:
+	// Overridden from zeek::plugin::Plugin.
+	zeek::plugin::Configuration Configure() override;
+};
+
+extern Plugin plugin;
+
+}
+}
